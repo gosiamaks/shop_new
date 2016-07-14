9743161bf43f3dd9c92a94e67c1e9c7b17616cf6
gosiamaks <malgorzata.maksimowicz@gmail.com>
2016-07-14 13:27:36 +0200

    Adds services

---
+++ lib/services/FetchProduct.rb
@@ -0,0 +1,10 @@
+module Shop
+  class FetchProducts
+    def call(product_id)
+      return unless product_id
+      PRODUCTS.find do|poduct|
+        product.id == product_id.to_i
+      end
+    end
+  end
+end
--- shop.rb
+++ shop.rb
@@ -1,5 +1,12 @@
-require relative './lib/model/product'
+Dir["./lib/**/*.rb"].each{|file| require file}
 
 module Shop
-  class Main
+  PRODUCTS = [
+    Product.new('t-shirt', 5),
+    Product.new('dress', 10),
+    Product.new('skirt', 12),
+    Product.new('jeans', 7),
+    Product.new('hoodie', 20)
+  ]
+
 end
