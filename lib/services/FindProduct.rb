7aafd6e79e557b88abc63a6ffdcb5d3627870eb5
gosiamaks <malgorzata.maksimowicz@gmail.com>
2016-07-17 17:22:48 +0200

    Shop with simple functions done; ugly but working. Didnt know how to show that qty of items in Warehouse decreases.

--- lib/services/FindProduct.rb
+++ lib/services/FindProduct.rb
@@ -2,7 +2,7 @@ module Shop
   class FindProduct
     def call(product_id)
       return unless product_id
-      PRODUCTS.find do|poduct|
+      PRODUCTS.find do|product|
         product.id == product_id.to_i
       end
     end
--- shop.rb
+++ shop.rb
@@ -9,7 +9,8 @@ module Shop
     Product.new('dress', 10),
     Product.new('skirt', 12),
     Product.new('jeans', 7),
-    Product.new('hoodie', 20)
+    Product.new('hoodie', 20),
+    Product.new('jacket', 16)
   ]
 
   WAREHOUSE = []
@@ -32,23 +33,31 @@ module Shop
     get "/products/:id" do |id|
       product = FindProduct.new.call(id)
       halt 404 unless product
+      erb :"products/show", locals: { product: product }
     end
 
     get "/warehouse" do
-      products = FetchWarehouseItems.new.call
+      items = FetchWarehouseItems.new.call
+      erb :"warehouse/index", locals: { items: items, title: "Warehouse" }
     end
 
     get "/basket" do
       basket = FetchBasketItems.new.call
+      sum_price = basket.map{|x| x[:total_price]}.reduce(:+)
+      erb :"basket/show", locals: { basket: basket, sum_price: sum_price }
     end
 
     post "/basket" do
       begin
         AddItemToBasket.new(params).call
-        redirect "/"
+        redirect "/basket"
       rescue KeyError
         halt 422
       end
+    end
+
+    get "/payment" do
+      erb :"basket/payment"
     end
   end
 end
